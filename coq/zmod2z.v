8b6b02fc6a05e942cd90bf9a450b31186207efe1
Amanda Liu <al3623@columbia.edu>
2019-10-09 22:48:08 -0400

    Add proof of group laws for Z/2Z

---
+++ coq/zmod2z.v
@@ -0,0 +1,57 @@
+Inductive Z2Z : Type :=
+    | O : Z2Z
+    | I : Z2Z.
+
+Definition plusZ2Z (x : Z2Z) (y : Z2Z) : Z2Z :=
+  match x with
+  | O => y
+  | I => match y with
+         | O => I
+         | I => O
+         end
+  end.
+
+Lemma id_com : forall x : Z2Z, plusZ2Z O x = plusZ2Z x O.
+Proof. intros x.
+       simpl.
+       destruct x.
+       - simpl.
+         reflexivity.
+       - simpl.
+         reflexivity.
+Qed.
+
+Theorem id : forall x : Z2Z, plusZ2Z O x = x.
+Proof. intros x.
+       simpl.
+       reflexivity.
+Qed.
+
+(* Proved existence of additive identity that commutes *)
+
+Theorem assoc : forall x y z : Z2Z, plusZ2Z (plusZ2Z x y) z 
+        = plusZ2Z x (plusZ2Z  y z).                                              
+ Proof. intros x y z.
+        destruct x.
+        - simpl.
+          reflexivity.
+        - destruct y.
+          + destruct z.
+            simpl.
+            reflexivity.
+            simpl.
+            reflexivity.
+          + destruct z.
+            simpl.
+            reflexivity.
+            simpl.
+            reflexivity.
+ Qed.
+
+ Theorem inv : forall x : Z2Z, exists y : Z2Z, plusZ2Z x y = O.
+ Proof. intros x.
+        destruct x.
+        - now exists O.
+        - now exists I.               
+   Qed.
+
