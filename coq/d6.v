7eb25472bec8e7b5ae0ad7d7fdbd54b42abd179a
Amanda Liu <al3623@columbia.edu>
2019-10-20 14:37:25 -0400

    Add tuple D6 representation

--- coq/d6.v
+++ coq/d6.v
@@ -1,3 +1,5 @@
+Load naturals.
+
 (* 
  * D6 = {1, r, r^2, s, sr, sr^2}
  * A dihedral group of symmetries on a three sided regular polygon.
@@ -6,3 +8,14 @@
  * 1 is the identity.
  * Other elements of the group comprise composition of rotations/reflections 
  *)
+
+Inductive Rot3 : Type :=
+   | rot0 : Rot3
+   | rot1 : Rot3
+   | rot2 : Rot3.
+
+Inductive Ref : Type :=
+   | ref0 : Ref
+   | ref1 : Ref.
+
+Definition D6 : Type := Ref * Rot3.
