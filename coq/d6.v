7ce859a53a39c1b83256de43977b61a6f2914540
Amanda Liu <al3623@columbia.edu>
2019-10-20 14:17:45 -0400

    Add D6 description

--- coq/d6.v
+++ coq/d6.v
@@ -1,1 +1,8 @@
-(* 1, r, r^2, s, sr, sr^2 *)
+(* 
+ * D6 = {1, r, r^2, s, sr, sr^2}
+ * A dihedral group of symmetries on a three sided regular polygon.
+ * r elements represent rotations.
+ * s elements represent reflection.
+ * 1 is the identity.
+ * Other elements of the group comprise composition of rotations/reflections 
+ *)
